711dfb63225ec4189a487c1263bc4c9f0cb69197
Douglas <mercurymuur86@yahoo.com>
2023-03-02 16:28:34 -0500

    pushing to gh-pages

---
+++ src/components/Custom.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Custom = (props) => {
+    const {countDown} = props
+
+    return (
+        <div>
+            <button onClick = {custom}>Math Custom</button>
+        </div>
+    )
+}
+
+export default Custom
